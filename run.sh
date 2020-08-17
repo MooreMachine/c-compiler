62ac531be69ebf1f1c7526aaad7bab3bf5b51cea
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2020-08-17 13:36:53 +0200

    master: Separate flags in run script.

--- run.sh
+++ run.sh
@@ -10,7 +10,10 @@ VERSION=$(cat README.md | grep "Version" | awk '{print $2}')
 
 cd $1
 
-docker run --rm -it \
+docker run \
+    --rm \
+    -i \
+    -t \
     -v $(pwd):/home \
     -w '/home' \
     ccompiler:$VERSION
